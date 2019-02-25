29b87cc74d83dd448438e293f22828e076f6c723
csPython <cspython@AD25TF04NGF1J-178.local>
2019-02-25 13:34:30 -0500

    Added Second Shape

--- shapes5AB.py
+++ shapes5AB.py
@@ -13,17 +13,3 @@ sSqr = Polygon(Point(50, 950), Point(200, 950), Point(200, 800), Point(50, 800))
 sSqr.setFill(color_rgb(100, 25, 21))
 sSqr.draw(s5Win)
 
-sPent = Polygon(Point(875, 950), Point(950, 875), Point(925, 800), Point(825, 800), Point(800, 875))
-sPent.setFill(color_rgb(0, 225, 0))
-sPent.draw(s5Win)
-
-sHex = Polygon(Point(832, 200), Point(917, 200), Point(950, 125), Point(917, 50), Point(832, 50), Point(800, 125))
-sHex.setFill(color_rgb(225, 67, 22))
-sHex.draw(s5Win)
-             
-sRum = Polygon(Point(500, 600), Point(550, 500), Point(500, 400), Point(450, 500))
-sRum.setFill(color_rgb(1, 189, 250))
-sRum.draw(s5Win)
-
-s5Win.getMouse()
-s5Win.close()
